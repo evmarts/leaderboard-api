f0bf596a51fd15ccc0e8bc62a3f12906539d153b
Evan Martin <evan.martin@shaw.ca>
2019-01-07 13:57:01 -0800

    deploying with support to postgres db

--- routes/api/leaderboard.js
+++ routes/api/leaderboard.js
@@ -6,12 +6,10 @@ const knex = require("../../database");
 router.use(cors());
 
 router.get("/", async (req, res) => {
-  console.log('HHHHHEEEEEYYYYYY')
-  // let r = await knex
-  //   .select("users_small.username", "users_small.points")
-  //   .from("users_small");
-  // res.send(r);
-  res.send('HEY')
+  let r = await knex
+    .select("users_small.username", "users_small.points")
+    .from("users_small");
+  res.send(r);
 });
 
 module.exports = router;
