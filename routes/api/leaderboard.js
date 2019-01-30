f2557a433595efc5eb01d83b38a65124bc8ac684
Evan Martin <evan.martin@shaw.ca>
2019-01-29 22:48:58 -0800

    moving helpers to one file

--- routes/api/leaderboard.js
+++ routes/api/leaderboard.js
@@ -1,14 +1,23 @@
 var express = require("express");
 var router = express.Router();
+router.use(cors());
 var cors = require("cors");
 const knex = require("../../database");
 var Client = require("instagram-private-api").V1;
-var storage = new Client.CookieFileStorage("./cookies/cookies.json");
-const Promise = require("bluebird");
+const {
+  getSesh,
+  getMedia,
+  getLikersOfMedia,
+  getUserIdFromUsername
+} = require("../../instagram-private-api/helpers.js");
 
-router.use(cors());
+// TODOs
+// todo: should consider creating a endpoint that updates a 'likes' table and do a select to that table here
+// todo: add try/catch blocks
 
+// GET api/leaderboard returns the current leaderboard
 router.get("/", async (req, res) => {
+  console.log("RECEIVED: GET /API/LEADERBOARD");
   let r = await knex
     .select("username", "points", "likes", "users_tagged", "support_a_creator")
     .from("leaderboard")
@@ -17,20 +26,19 @@ router.get("/", async (req, res) => {
   res.send(r);
 });
 
+// PATCH api/leaderboard
 router.patch("/", async (req, res) => {
-  console.log("receieved a patch request with body:", req.body);
-  const USER_CREDS = {
-    acc1: {
+  // SETUP
+  console.log("RECEIVED: PATCH /API/LEADERBOARD");
+  var device = new Client.Device(req.body.user);
+  const session = await getSesh(
+    {
       username: req.body.user,
-      password: req.body.password,
-      target: req.body.target
-    }
-  };
-
-  // set up
-  var device = new Client.Device(USER_CREDS.acc1.username);
-  const session = await getSesh(USER_CREDS.acc1, device);
-  const accountName = USER_CREDS.acc1.target;
+      password: req.body.password
+    },
+    device
+  );
+  const accountName = req.body.target;
   const accountID = await getUserIdFromUsername(session, accountName);
 
   // get all the medias of the account after the specified start date
@@ -38,8 +46,6 @@ router.patch("/", async (req, res) => {
     .filter(m => m._params.takenAt > parseInt(req.body.startDate))
     .map(m => m.id);
 
-  // todo should consider creating a endpoint that updates a 'likes'
-  //    table and do a select to that table here
   // get all likers of your media
   let likersMap = {};
   for (mid of mediaIds) {
@@ -56,9 +62,9 @@ router.patch("/", async (req, res) => {
   for (user of Object.entries(likeObj)) {
     rowsLikes.push({ username: user[0], likes: user[1] });
   }
-  // clear likes table
+
+  // clear 'likes' table then insert new rows into likes table
   await knex("likes").del();
-  // insert into likes table
   await knex("likes").insert(rowsLikes);
 
   // grab all the comment tuples that are validated
@@ -67,23 +73,24 @@ router.patch("/", async (req, res) => {
     .where("validated", true);
   let taggerMap = {};
 
-  // create a dic that counts number of occurences each user has tagged a friend
+  // create a object array that counts number of occurences each user has
+  // tagged a friend
   for (comment of validComments) {
     let u = comment.username;
     taggerMap[u] = taggerMap[u] ? taggerMap[u] + 1 : 1;
   }
 
-  // reset the leaderboard
+  // reset the leaderboard and insert new rows of likers
   await knex.raw("delete from leaderboard where 1 = 1");
-
-  // insert rows of likers
   await knex("leaderboard").insert(rowsLikes);
 
+  // get the the number of tagged friends each user has done
   const rowsComments = await knex.raw(
     "select username, count(*) as tagged from tagged group by username;"
   );
-  console.log('here 1')
 
+  // for each (username, tagCount) tuple, update the user's tagged column if they
+  // already have a record OR create a new record if they don't already have a record
   for (row of rowsComments.rows) {
     await knex.raw(`insert into leaderboard (username, users_tagged) values ('${
       row.username
@@ -93,8 +100,7 @@ router.patch("/", async (req, res) => {
     }' where leaderboard.username = '${row.username}'`);
   }
 
-  console.log('here 2')
-
+  // insert any support a creator records into the leaderboard
   await knex.raw(
     `
     update leaderboard
@@ -104,83 +110,12 @@ router.patch("/", async (req, res) => {
     `
   );
 
-  console.log('here 3')
-
-  console.log(
-    await knex.raw(
-      "UPDATE leaderboard SET points = 100*likes + 200*users_tagged + 500*is_supporter"
-    )
+  // calculate points from the other columns in the leaderboard
+  await knex.raw(
+    "UPDATE leaderboard SET points = 100*likes + 200*users_tagged + 500*is_supporter"
   );
 
-  console.log('here 4')
-
-  res.send("response here");
+  res.sendStatus(200);
 });
-
-const getComments = async (session, mediaID) => {
-  let feed = new Client.Feed.MediaComments(session, mediaID);
-  let comments = await new Promise((resolve, reject) => {
-    resolve(feed.all());
-  });
-  let coms = [];
-  for (c of comments) {
-    coms.push(c);
-  }
-  return coms;
-};
-
-async function getUserIdFromUsername(session, username) {
-  return Client.Account.searchForUser(session, username)
-    .then(account => {
-      return account.id;
-    })
-    .catch(err => console.error(err.message));
-}
-
-function union_arrays(x, y) {
-  var obj = {};
-  for (var i = x.length - 1; i >= 0; --i) obj[x[i]] = x[i];
-  for (var i = y.length - 1; i >= 0; --i) obj[y[i]] = y[i];
-  var res = [];
-  for (var k in obj) {
-    if (obj.hasOwnProperty(k))
-      // <-- optional
-      res.push(obj[k]);
-  }
-  return res;
-}
-
-const extractUserNames = string => {
-  const matches = [];
-  for (s of string.split(" ")) {
-    const re = /(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/;
-    if (s.match(re)) {
-      matches.push(s.match(re)[1]);
-    }
-  }
-  return matches;
-};
-
-// gets the likers of a media
-const getLikersOfMedia = async (session, mediaID) => {
-  return await new Promise((resolve, reject) => {
-    resolve(Client.Media.likers(session, mediaID));
-  });
-};
-
-const getSesh = async (acc, device) => {
-  return await new Promise((resolve, reject) => {
-    resolve(Client.Session.create(device, storage, acc.username, acc.password));
-  });
-};
-
-const getMedia = async (session, userID) => {
-  return await new Promise((resolve, reject) => {
-    let feed = new Client.Feed.UserMedia(session, userID);
-    feed.all().then(data => {
-      resolve(data);
-    });
-  });
-};
 
 module.exports = router;
