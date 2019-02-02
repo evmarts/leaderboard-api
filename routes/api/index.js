608bb8ca5cd4d4c380e19c276916f39aca40f293
Evan Martin <evan.martin@shaw.ca>
2019-02-02 00:22:08 -0800

    remove rl

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,13 +4,6 @@ const rateLimit = require("express-rate-limit");
 var leaderboardRoute = require("./leaderboard");
 var commentRoute = require("./comments");
 var searchRoute = require("./search");
-var cors = require("cors");
-
-router.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -18,9 +11,7 @@ const apiLimiter = rateLimit({
 });
 
 // rate limiter
-router.enable("trust proxy");
 router.use("/search", apiLimiter);
-router.use(cors());
 
 router.use("/leaderboard", leaderboardRoute);
 router.use("/comments", commentRoute);
